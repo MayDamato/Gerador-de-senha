cc97fbb41d94671d3762d2625dcaf927407e655e
MayDamato <maydamato@gmail.com>
2023-08-25 13:43:04 -0300

    Projeto Gerador de senhas

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassoword = document.querySelector("#container-passoword");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789*@#";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function () {
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+
+    containerPassoword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyPassword() {
+
+    navigator.clipboard.writeText(novaSenha);
+    alert("Senha copiada com sucesso!")
+
+}
+
+
+
